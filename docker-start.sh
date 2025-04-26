1f0e3ed5a433a598ca47f83c1c3a714379160003
Sai Kowshik Ananthula <ananthulasaikowshik@gmail.com>
2025-04-26 13:58:34 -0500

    enhanced a bit

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,11 @@
 docker build -t financial-news-monitor .
 
-docker run -d --name financial-monitor --env-file .env -v ./database:/app/database -v ./logs:/app/logs -v ./backups:/app/backups financial-news-monitor
+docker run -d --name financial-monitor \
+  --env-file .env \
+  -v ./database:/app/database \
+  -v ./logs:/app/logs \
+  -v ./backups:/app/backups \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  --log-opt max-file=3 \
+  financial-news-monitor
