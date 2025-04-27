ba2f62628933fa26cf4b091f0c4ae46de36f3fed
Sai Kowshik Ananthula <ananthulasaikowshik@gmail.com>
2025-04-27 01:32:52 -0500

    enhanced a bit

--- docker-start.sh
+++ docker-start.sh
@@ -2,10 +2,10 @@ docker build -t financial-news-monitor .
 docker stop financial-monitor
 docker rm financial-monitor
 docker run -d --name financial-monitor \
-  --env-file .env \
-  -v ./database:/app/database \
-  -v ./logs:/app/logs \
-  -v ./backups:/app/backups \
+  -e TELEGRAM_API_KEY -e OPENAI_API_KEY \
+  -v ./database:/home/appuser/app/database \
+  -v ./logs:/home/appuser/app/logs \
+  -v ./backups:/home/appuser/app/backups \
   --log-driver json-file \
   --log-opt max-size=10m \
   --log-opt max-file=3 \
